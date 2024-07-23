0d5df8db45db66dde72371c1088dc3ba02287875
Arnav <48650781+arnavrneo@users.noreply.github.com>
2024-07-23 12:34:39 +0530

    update/wrap-up

--- sql/3_prod_views.sql
+++ sql/3_prod_views.sql
@@ -1,105 +1,174 @@
-USE ROLE accountadmin;
+use database nifty50db;
+use schema prod;
 
--- Creating external stage
-CREATE or REPLACE STORAGE INTEGRATION aws_stage_data
-    TYPE = EXTERNAL_STAGE
-    STORAGE_PROVIDER = S3
-    ENABLED = TRUE
-    STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::211125565587:role/snowflake-aws-role'
-    STORAGE_ALLOWED_LOCATIONS = ('s3://niftyde-analytics/');
+-- BLOCK DEALS
+-- 1) Top 5 block deals
+CREATE or REPLACE VIEW prod.top_block_deals AS 
+select
+    timestamp,
+    symbol,
+    client_name,
+    qty_traded,
+    buy_sell,
+    trading_price
+from dev.block_deals
+where timestamp = (select max(timestamp) from dev.block_deals)
+order by qty_traded DESC
+limit 10;
 
--- Describe the integration object
--- Check Row 5 and Row 7
-DESC INTEGRATION aws_stage_data;
-
--- Grant usage access to Sysadmin Role ---
-GRANT usage ON INTEGRATION aws_stage_data TO ROLE sysadmin;
-
--- Create a file format  ---
-USE DATABASE nifty50db;
-USE SCHEMA dev;
-CREATE or REPLACE FILE FORMAT csv_load_format
-    TYPE = 'CSV' 
-    COMPRESSION = 'AUTO' 
-    FIELD_DELIMITER = ',' 
-    RECORD_DELIMITER = '\n' 
-    SKIP_HEADER =1 
-    FIELD_OPTIONALLY_ENCLOSED_BY = '\042' 
-    TRIM_SPACE = FALSE 
-    ERROR_ON_COLUMN_COUNT_MISMATCH = TRUE 
-    ESCAPE = 'NONE' 
-    ESCAPE_UNENCLOSED_FIELD = '\134' 
-    DATE_FORMAT = 'AUTO' 
-    TIMESTAMP_FORMAT = 'AUTO';
+-- 2) Total buy and sell with max of each grouped by timestamp
+CREATE or REPLACE VIEW prod.total_block_buy_sell AS
+select
+    timestamp,
+    COUNT(CASE WHEN buy_sell = 'BUY' THEN buy_sell END) AS total_buy,
+    COUNT(CASE WHEN buy_sell = 'SELL' THEN buy_sell END) AS total_sell,
+    MAX(CASE WHEN buy_sell = 'BUY' THEN qty_traded END) AS max_buy,
+    MAX(CASE WHEN buy_sell = 'SELL' THEN qty_traded END) AS max_sell
+from dev.block_deals
+where timestamp = (select max(timestamp) from dev.block_deals)
+GROUP BY timestamp;
 
 
--- Create stage for each table
-CREATE or REPLACE STAGE stg_block_deals
-storage_integration = aws_stage_data
-url = 's3://niftyde-analytics/blockdealarch/'
-file_format = csv_load_format;
+-- BOARD MEETINGS
+-- when, by whom and why
+CREATE or REPLACE VIEW prod.board_meeting AS
+select
+    timestamp,
+    company_name,
+    purpose
+from dev.board_meetings
+order by timestamp DESC
+limit 10;
 
-CREATE or REPLACE STAGE stg_board_meetings
-storage_integration = aws_stage_data
-url = 's3://niftyde-analytics/boardmeetings/'
-file_format = csv_load_format;
+-- BULK DEALS
+-- 1) Top 5 bulk deals
+CREATE or REPLACE VIEW prod.top_bulk_deals AS
+select
+    timestamp,
+    symbol,
+    client_name,
+    qty_traded,
+    buy_sell,
+    trading_price
+from dev.bulk_deals
+where timestamp = (select max(timestamp) from dev.bulk_deals)
+order by qty_traded DESC
+limit 10;
 
-CREATE or REPLACE STAGE stg_bulk_deals
-storage_integration = aws_stage_data
-url = 's3://niftyde-analytics/bulkdealarch/'
-file_format = csv_load_format;
+-- 2) Total buy and sell with max of each grouped by timestamp
+CREATE or REPLACE VIEW prod.max_bulk_buy_sell AS
+select
+    timestamp,
+    COUNT(CASE WHEN buy_sell = 'BUY' THEN buy_sell END) AS total_buy,
+    COUNT(CASE WHEN buy_sell = 'SELL' THEN buy_sell END) AS total_sell,
+    MAX(CASE WHEN buy_sell = 'BUY' THEN qty_traded END) AS max_buy,
+    MAX(CASE WHEN buy_sell = 'SELL' THEN qty_traded END) AS max_sell
+from dev.bulk_deals
+where timestamp = (select max(timestamp) from dev.bulk_deals)
+GROUP BY timestamp;
 
-CREATE or REPLACE STAGE stg_monthly_adv_dec
-storage_integration = aws_stage_data
-url = 's3://niftyde-analytics/monthlyadvdec/'
-file_format = csv_load_format;
+-- monthly advances and declines
+-- 1) month wise results
+CREATE or REPLACE VIEW prod.month_wise_adv_dec AS
+select 
+    timestamp,
+    advances,
+    declines
+from dev.monthly_adv_dec
+order by timestamp DESC;
 
-CREATE or REPLACE STAGE stg_nse_ratios
-storage_integration = aws_stage_data
-url = 's3://niftyde-analytics/indexratios/'
-file_format = csv_load_format;
+-- NSE RATIOS
+-- 1) open/close
+CREATE or REPLACE VIEW prod.nse_open_close AS
+select
+    timestamp,
+    open,
+    close
+from dev.nse_ratios
+order by timestamp ASC;
 
-CREATE or REPLACE STAGE stg_sec_archives
-storage_integration = aws_stage_data
-url = 's3://niftyde-analytics/secarch/'
-file_format = csv_load_format;
-
-CREATE or REPLACE STAGE stg_short_sell_arch
-storage_integration = aws_stage_data
-url = 's3://niftyde-analytics/shortsellarch/'
-file_format = csv_load_format;
-
--- List the data
-LIST@stg_bulk_deals;
-LIST@stg_block_deals;
-LIST@stg_monthly_adv_dec;
+-- 2) high/low
+CREATE or REPLACE VIEW prod.nse_high_low AS
+select
+    timestamp,
+    high,
+    low
+from dev.nse_ratios
+order by timestamp ASC;
 
 
--- Creating a pipeline
+-- SHORT SELL ARCH
+CREATE or REPLACE VIEW prod.short_sell AS
+select
+    timestamp,
+    symbol,
+    qty_short_sold
+from dev.short_sell_arch
+order by timestamp ASC;
 
-CREATE or REPLACE PIPE block_deals_pipe AUTO_INGEST=true AS
-COPY INTO BLOCK_DEALS FROM @stg_block_deals ON_ERROR = continue;
+-- SEC ARCHIVES
+-- 1) lineplot: high and low
+CREATE or REPLACE VIEW prod.sec_high_low AS
+select
+    timestamp,
+    symbol,
+    trade_high_price,
+    trade_low_price
+from dev.sec_archives
+where timestamp = (select max(timestamp) from dev.sec_archives)
+order by timestamp ASC;
 
-CREATE or REPLACE PIPE board_meetings_pipe AUTO_INGEST=true AS
-COPY INTO BOARD_MEETINGS FROM @stg_board_meetings ON_ERROR = continue;
+-- 2) lineplot: open and close
+CREATE or REPLACE VIEW prod.sec_open_close AS
+select
+    timestamp,
+    symbol,
+    trade_open_price,
+    trade_close_price
+from dev.sec_archives
+where timestamp = (select max(timestamp) from dev.sec_archives)
+order by timestamp ASC;
 
-CREATE or REPLACE PIPE bulk_deals_pipe AUTO_INGEST=true AS
-COPY INTO BULK_DEALS FROM @stg_bulk_deals ON_ERROR = continue;
+-- 3) barplot: total traded val in crores
+CREATE or REPLACE VIEW prod.sec_total_traded_value AS
+select
+    timestamp,
+    symbol,
+    total_traded_val_in_crores,
+from dev.sec_archives
+where timestamp = (select max(timestamp) from dev.sec_archives)
+order by total_traded_val_in_crores DESC
+limit 10;
 
-CREATE or REPLACE PIPE monthly_adv_dec_pipe AUTO_INGEST=true AS
-COPY INTO MONTHLY_ADV_DEC FROM @stg_monthly_adv_dec ON_ERROR = continue;
+-- 4) total traded quantity
+CREATE or REPLACE VIEW prod.sec_total_traded_qty AS
+select
+    timestamp,
+    symbol,
+    total_trading_quant,
+from dev.sec_archives
+where timestamp = (select max(timestamp) from dev.sec_archives)
+order by total_trading_quant DESC
+limit 10;
 
-CREATE or REPLACE PIPE nse_ratios_pipe AUTO_INGEST=true AS
-COPY INTO NSE_RATIOS FROM @stg_nse_ratios ON_ERROR = continue;
+-- 5) current closing - previous closing
+CREATE or REPLACE VIEW prod.sec_cur_prev_close AS
+select
+    timestamp,
+    symbol,
+    (trade_close_price - previous_closing_price) AS diff_curr_close_prev_close
+from dev.sec_archives
+where timestamp = (select max(timestamp) from dev.sec_archives)
+order by timestamp ASC
+limit 10;
 
-CREATE or REPLACE PIPE sec_archives_pipe AUTO_INGEST=true AS
-COPY INTO SEC_ARCHIVES FROM @stg_sec_archives ON_ERROR = continue;
-
-CREATE or REPLACE PIPE short_sell_arch_pipe AUTO_INGEST=true AS
-COPY INTO SHORT_SELL_ARCH FROM @stg_short_sell_arch ON_ERROR = continue;
-
-SHOW pipes;
-
--- truncate table block_deals;
-
--- SELECT * FROM block_deals;
+-- 6) 52week high and low
+CREATE or REPLACE VIEW prod.sec_52week AS
+select
+    timestamp,
+    symbol,
+    high_52week,
+    low_52week
+from dev.sec_archives
+where timestamp = (select max(timestamp) from dev.sec_archives)
+order by timestamp ASC;
--- tf/0_providers.tf
+++ tf/0_providers.tf
@@ -7,6 +7,6 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-east-1"
-  profile = "arnav-first-1"
+  region  = "<REGION>"
+  profile = "<AWS-PROFILE>"
 }
